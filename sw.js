b6ac1987f1cff60b55711dd018f6d62cc66ccf47
matiaszazkin-collab <matiaszazkin@gmail.com>
2026-08-24 00:10:57 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+/* Cachea la app para que abra sin conexión. Los datos viven en localStorage, no acá. */
+var CACHE = "ficha-nutricional-v2";
+var ARCHIVOS = [
+  "./", "./index.html", "./manifest.webmanifest",
+  "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"
+];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ARCHIVOS); }).catch(function(){}));
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.map(function(k){ return k===CACHE ? null : caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e){
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      if(hit) return hit;
+      return fetch(e.request).then(function(res){
+        var copia = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, copia); }).catch(function(){});
+        return res;
+      }).catch(function(){ return caches.match("./index.html"); });
+    })
+  );
+});
